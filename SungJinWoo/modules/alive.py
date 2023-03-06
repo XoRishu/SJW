4f0684193594459fdefd5ed47964c046f4a2d0ad
XoRishu <115208147+XoRishu@users.noreply.github.com>
2023-03-06 16:04:29 +0530

    Update alive.py

--- SungJinWoo/modules/alive.py
+++ SungJinWoo/modules/alive.py
@@ -3,11 +3,11 @@
 from telethon import events, Button, custom
 from SungJinWoo.events import register
 from SungJinWoo import telethn as tbot
-SungJinWoo = "https://telegra.ph/file/9dfcdab5244a61b323210.jpg"
+SungJinWoo = "https://telegra.ph/file/89229391b9e7733e4fba5.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   STB = event.sender.first_name
-  STB = "**I m SungJinWoo** \n\n" + "**I'm Working Properly**\n\n"
+  STB = "**I m Kanna Kamui** \n\n" + "**I'm Working Properly**\n\n"
   STB += "**Python Version : 3.9.7**\n\n"
   STB += "**python-Telegram-Bot : 13.7**\n\n"
   BUTTON = [[Button.url("Support", "https://t.me/{SUPPORT_CHAT}"), Button.url("Updates", "https://t.me/{UPDATES_CHANNEL}")]]
